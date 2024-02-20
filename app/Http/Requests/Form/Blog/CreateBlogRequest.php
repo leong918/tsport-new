384205f6fde6864d620d75e6aa49f7cf8a1dd083
Desmond Lee <fuquan.lee@vvinners.com>
2024-02-20 10:34:19 +0800

    update

--- app/Http/Requests/Form/Blog/CreateBlogRequest.php
+++ app/Http/Requests/Form/Blog/CreateBlogRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Form\Blog;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class CreateBlogRequest extends FormRequest
 {
@@ -29,5 +31,15 @@ class CreateBlogRequest extends FormRequest
             'sort' => 'required',
             'published_at' => 'required|date_format:d/m/Y',
         ];
+    }
+
+    // Returning errors as exception
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'msg' => $validator->errors()->first(),
+            ], 500)
+        );
     }
 }
