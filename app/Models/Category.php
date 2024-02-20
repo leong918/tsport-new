ffbaa5ce829f017c0c8eb567d9021b0b1537f755
Desmond Lee <fuquan.lee@vvinners.com>
2024-02-20 12:34:52 +0800

    update: category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,6 +17,17 @@ class Category extends Model
      */
     public static $rules = [];
 
+    public const STATUS = [
+        'ACTIVE' => 1,
+        'INACTIVE' => 0,
+    ];
+
+    public const TYPE = [
+        'SKIN CARE' => 'skin_care',
+        'MAKEUP' => 'makeup',
+        'HAIR & BODY' => 'hair_body'
+    ];
+
     protected $table = 'category';
 
     /**
