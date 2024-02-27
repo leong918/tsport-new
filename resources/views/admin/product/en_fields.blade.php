9bbbe5523e31c071a1b2344f2ebf45964dd8bce4
Jeff Chua <chuazile1998@gmail.com>
2024-02-27 14:26:30 +0800

    update

--- resources/views/admin/product/en_fields.blade.php
+++ resources/views/admin/product/en_fields.blade.php
@@ -1,56 +1,38 @@
-<div>
-    <table class="table table-striped border">
-        <tbody >
-            <tr>
-                <tr>
-                    <td scope="col">{{__('page.name')}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        {{ html()->text('language[en][name]')->value(isset($model) && $model->enDescription ? $model->enDescription->name : "")->placeholder('Enter name')->class('form-control')->required() }}
-                    </td>
-                </tr>
-                <tr>
-                    <td scope="col">{{__('page.information')}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        {{ html()->textarea('language[en][information]')->value(isset($model) && $model->enDescription ? $model->enDescription->information : "")->class('form-control wysiwyg') }}
-                    </td>
-                </tr>
-                <tr>
-                    <td scope="col">{{__('page.description')}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        {{ html()->textarea('language[en][description]')->value(isset($model) && $model->enDescription ? $model->enDescription->description : "")->class('form-control wysiwyg') }}
-                    </td>
-                </tr>
-                <tr>
-                    <td scope="col">{{__('page.ingredient')}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        {{ html()->textarea('language[en][ingredient]')->value(isset($model) && $model->enDescription ? $model->enDescription->ingredient : "")->class('form-control wysiwyg') }}
-                    </td>
-                </tr>
-                <tr>
-                    <td scope="col">{{__('page.usage')}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        {{ html()->textarea('language[en][usage]')->value(isset($model) && $model->enDescription ? $model->enDescription->usage : "")->class('form-control wysiwyg') }}
-                    </td>
-                </tr>
-                <tr>
-                    <td scope="col">{{__('page.additional_information')}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        {{ html()->textarea('language[en][additional_information]')->value(isset($model) && $model->enDescription ? $model->enDescription->additional_information : "")->class('form-control wysiwyg') }}
-                    </td>
-                </tr>
-            </tr>
-        </tbody>
-    </table>
+<div class="row">
+    <div class="col-md-12">
+        <div class="mb-3">
+            {{ html()->label('Name') }}
+            {{ html()->text('language[en][name]')->value(isset($model) && $model->enDescription ? $model->enDescription->name : "")->placeholder('Enter name')->class('form-control')->required() }}
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="mb-3">
+            {{ html()->label('Information') }}
+            {{ html()->textarea('language[en][information]')->value(isset($model) && $model->enDescription ? $model->enDescription->information : "")->class('form-control wysiwyg') }}
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="mb-3">
+            {{ html()->label('Description') }}
+            {{ html()->textarea('language[en][description]')->value(isset($model) && $model->enDescription ? $model->enDescription->description : "")->class('form-control wysiwyg') }}
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="mb-3">
+            {{ html()->label('Ingredient') }}
+            {{ html()->textarea('language[en][ingredient]')->value(isset($model) && $model->enDescription ? $model->enDescription->ingredient : "")->class('form-control wysiwyg') }}
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="mb-3">
+            {{ html()->label('Usage') }}
+            {{ html()->textarea('language[en][usage]')->value(isset($model) && $model->enDescription ? $model->enDescription->usage : "")->class('form-control wysiwyg') }}
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="mb-3">
+            {{ html()->label('Additional Information') }}
+            {{ html()->textarea('language[en][additional_information]')->value(isset($model) && $model->enDescription ? $model->enDescription->additional_information : "")->class('form-control wysiwyg') }}
+        </div>
+    </div>
 </div>
