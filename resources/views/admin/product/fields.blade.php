3fd490f42429879a0a162aaf9a73f69e59bfc179
Jeff Chua <chuazile1998@gmail.com>
2024-03-18 13:48:06 +0800

    update fields

--- resources/views/admin/product/fields.blade.php
+++ resources/views/admin/product/fields.blade.php
@@ -169,7 +169,7 @@
 
 <script>
     $(document).ready(function() { 
-        $('#product-related-select, #product_tag_select').select2({
+        $('#product-related-select, #product-tag-select').select2({
             theme: "bootstrap-5",
             width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
             placeholder: $(this).data('placeholder'),
