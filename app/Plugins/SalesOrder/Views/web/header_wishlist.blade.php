da2345cbfe8072dc8781b60bcb108adf9ce5c3ad
Jeff Chua <chuazile1998@gmail.com>
2024-03-22 15:41:31 +0800

    update header wishlist

---
+++ app/Plugins/SalesOrder/Views/web/header_wishlist.blade.php
@@ -0,0 +1,3 @@
+<a class="navbar-wishlist" href="{{route('cart.wishlist')}}" type="button">
+    <img src="{{asset('assets/web/assets/img/navigation/wishlist-icon.png')}}" alt="Bootstrap" width="25" height="24">
+</a>
