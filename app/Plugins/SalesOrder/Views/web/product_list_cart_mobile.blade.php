8517fea8430ca314b32dd22599993f251a15df3a
Jeff Chua <chuazile1998@gmail.com>
2024-04-17 13:39:45 +0800

    done out of stock and adjust wishlist

--- app/Plugins/SalesOrder/Views/web/product_list_cart_mobile.blade.php
+++ app/Plugins/SalesOrder/Views/web/product_list_cart_mobile.blade.php
@@ -1,9 +1,15 @@
-@if(count($product->productAttribute) > 0)
-<div class="cart-container cart-button-redirect" href="{{route('web.product_detail', ['alias' => $product->alias])}}">
-    <img class="cart-mobile" src="{{asset('assets/web/assets/img/homepage/add-cart-2.png')}}">
+<div class="d-flex">
+    <div class="wishlist-container wishlist-button-hover-mobile me-2" data-is-wishlist="{{ $product->checkWishlist($product->id) ? 1 : 0 }}" data-id="{{ $product->id }}" data-url="{{ route('cart.toggle_wishlist') }}">
+        <img class="wishlist-mobile {{ $product->checkWishlist($product->id) ? 'd-none' : '' }}" src="{{ asset('assets/web/assets/img/homepage/add-wishlist-2.png') }}">
+        <img class="wishlist-mobile-added {{ $product->checkWishlist($product->id) ? '' : 'd-none' }}" src="{{asset('assets/web/assets/img/homepage/added_to_wishlist.png')}}">
+    </div>
+    @if(count($product->productAttribute) > 0)
+    <div class="cart-container cart-button-redirect" href="{{route('web.product_detail', ['alias' => $product->alias])}}">
+        <img class="cart-mobile" src="{{asset('assets/web/assets/img/homepage/add-cart-2.png')}}">
+    </div>
+    @else
+    <div class="cart-container cart-button-hover" data-id="{{ $product->id }}" data-url="{{ route('cart.add_to_cart') }}">
+        <img class="cart-mobile" src="{{asset('assets/web/assets/img/homepage/add-cart-2.png')}}">
+    </div>
+    @endif
 </div>
-@else
-<div class="cart-container cart-button-hover" data-id="{{ $product->id }}" data-url="{{ route('cart.add_to_cart') }}">
-    <img class="cart-mobile" src="{{asset('assets/web/assets/img/homepage/add-cart-2.png')}}">
-</div>
-@endif
--- app/Plugins/SalesOrder/Views/web/product_list_wishlist_mobile.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="wishlist-container wishlist-button-hover-mobile" data-is-wishlist="{{ $product->checkWishlist($product->id) ? 1 : 0 }}" data-id="{{ $product->id }}" data-url="{{ route('cart.toggle_wishlist') }}">
-    <img class="wishlist-mobile {{ $product->checkWishlist($product->id) ? 'd-none' : '' }}" src="{{ asset('assets/web/assets/img/homepage/add-wishlist-2.png') }}">
-    <img class="wishlist-mobile-added {{ $product->checkWishlist($product->id) ? '' : 'd-none' }}" src="{{asset('assets/web/assets/img/homepage/added_to_wishlist.png')}}">
-</div>
