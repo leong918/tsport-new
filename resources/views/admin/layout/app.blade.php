6f15bfc3c197548ab37430654962e21b3762a4ea
chuazile1998 <chuazile1998@gmail.com>
2024-04-19 16:11:09 +0800

    update

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="description" content="Ecommerce Boilerplate Admin Template">
-    <meta name="author" content="VVinners">
+    <meta name="author" content="Tag Concept">
     <meta name="keyword" content="Bootstrap,Admin,Template,SCSS,HTML,RWD,Dashboard">
-    <title>Ecommerce Boilerplate Admin Template</title>
+    <title>Tag Concept</title>
     <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
       integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
