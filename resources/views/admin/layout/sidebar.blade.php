1d5e28c64f4a87a72d8cc8f814f09c9e83194aac
Kai Qing Pang <pangkaiqing2000@gmail.com>
2024-07-22 18:20:31 +0800

    update admin siderbar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -3,9 +3,18 @@
         Hello Smile
     </div>
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar>
-        <li class="nav-item"><a class="nav-link" href="{{ route('admin.dashboard') }}">
-            <i class="fa-solid fa-chart-bar nav-icon"></i>
-             Dashboard</a></li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.dashboard') }}">
+                <i class="fa-solid fa-chart-bar nav-icon"></i>
+                Dashboard
+            </a>
+        </li>
+        <li class="nav-title">Shop</li>
+        <li class="nav-group">
+            <a class="nav-link" href="{{ route('admin.blog.index') }}">
+                <i class="fa-solid fa-chart-bar nav-icon"></i> Blog
+            </a>
+        </li>
     </ul>
     <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
 </div>
