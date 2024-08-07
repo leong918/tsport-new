1c061e84f6a28d80b453612a7942910d45b0ce04
Jeff Chua <chuazile1998@gmail.com>
2024-08-07 10:26:43 +0800

    update header

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,6 +1,6 @@
 <header class="header header-sticky mb-4">
     <div class="container-fluid">
-        <div class="left-content d-flex align-items-center">
+        <div class="left-content d-flex align-items-center justify-content-center">
             <button class="header-toggler px-md-0 me-md-3" type="button" onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()">
                 <i class="fa-solid fa-bars"></i>
             </button>
