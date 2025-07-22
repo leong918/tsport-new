6ca413a457410e7633a76379f7e1d916a856cbd5
Jeff Chua <chuazile1998@gmail.com>
2025-07-22 17:54:53 +0800

    init index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -3,18 +3,20 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Display Web">
     <title>Web</title>
-    <link type="text/css" rel="stylesheet" href="{{ mix('assets/web/default/app.css') }}" />
 </head>
 
 <body>
+    @vite(['resources/scss/web/app.scss', 'resources/js/web/app.js'])
     <div id="app">
         web index
     </div>
 
-    <script type="text/javascript" src="{{ mix('assets/web/app.js') }}"></script>
+    <script type="text/javascript"></script>
 </body>
 
 </html>
