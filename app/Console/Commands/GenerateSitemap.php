e16e88c34c2d55bc01212849f37cee756b74d889
Jeff Chua <chuazile1998@gmail.com>
2025-07-22 13:47:48 +0800

    update sitemap generator

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Sitemap via command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(env('APP_URL'))->getSitemap()->writeToDisk('sitemap', 'sitemap.xml');
+    }
+}
