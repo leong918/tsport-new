5e8a9c0612cdd7b6ecb37a12423d12834d6cb0eb
Jeff Chua <chuazile1998@gmail.com>
2025-07-25 16:15:17 +0800

    update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,38 +4,39 @@ namespace App\Http\Controllers\API;
 
 use App\Exceptions\GeneralException;
 use App\Http\Requests\API\UserLoginRequest;
-use App\Repositories\UserRepository;
+// use App\Repositories\UserRepository;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends BaseController
 {
-    private $userRepository;
+    // sample for api login
+    // private $userRepository;
 
-    public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
+    // public function __construct(UserRepository $userRepository)
+    // {
+    //     $this->userRepository = $userRepository;
+    // }
 
-    public function login(UserLoginRequest $request)
-    {
-        $user = $this->userRepository->getUserByUsername($request->username);
-        if (!$user) {
-            throw new GeneralException('USER_NOT_FOUND');
-        }
+    // public function login(UserLoginRequest $request)
+    // {
+    //     $user = $this->userRepository->getUserByUsername($request->username);
+    //     if (!$user) {
+    //         throw new GeneralException('USER_NOT_FOUND');
+    //     }
 
-        // check password match
-        if (Hash::check($request->password, $user->password)) {
-            // generate new session_id
-            $api_token = $this->userRepository->updateSession($user->id);
-            if ((bool) $api_token) {
-                $user->api_token = $api_token;
+    //     // check password match
+    //     if (Hash::check($request->password, $user->password)) {
+    //         // generate new session_id
+    //         $api_token = $this->userRepository->updateSession($user->id);
+    //         if ((bool) $api_token) {
+    //             $user->api_token = $api_token;
 
-                return $this->response(['data' => $user]);
-            }
-        } else {
-            throw new GeneralException('INVALID_PASSWORD');
-        }
+    //             return $this->response(['data' => $user]);
+    //         }
+    //     } else {
+    //         throw new GeneralException('INVALID_PASSWORD');
+    //     }
 
-        throw new GeneralException('LOGIN_FAILED');
-    }
+    //     throw new GeneralException('LOGIN_FAILED');
+    // }
 }
